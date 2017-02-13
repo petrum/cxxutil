e1afbf139f427e0893a88b28d5c93a5fe052fdda
Petru Marginean <pmarginean@laurioncap.com>
2017-02-13 14:43:28 -0500

    misc

--- main.cpp
+++ main.cpp
@@ -3,6 +3,17 @@
 #include "filelog.h"
 #include "alog.h"
 
+#include <pthread.h>
+#include <sched.h>
+
+void accelerate()
+{
+    int policy = SCHED_RR;
+    sched_param param;
+    param.sched_priority = sched_get_priority_max(policy);
+    ENFORCE(pthread_setschedparam(pthread_self(), policy, &param) == 0);
+}
+
 bool foo(bool b)
 {
     FILE_LOG(logINFO) << "foo(" << b << ")";
@@ -15,15 +26,16 @@ ALog* const ALog::pALog = &aLog;
 int main(int argc, char* argv[])
 {
     STD_FUNCTION_BEGIN;
+    accelerate();
     SCOPE_EXIT(foo(true));
     ALog::get().init(1000000, 256, "/tmp/test.log");
     SCOPE_EXIT(ALog::get().stop());
-    std::size_t NUM = 1;
+    std::size_t NUM = 10;
     FILE_LOG(logINFO) << "Started logging " << NUM << " messages";
     for (std::size_t i = 0; i != NUM; ++i)
     {
         ALOG << "Hello" << " World " << 3.2 << " blabla " << i;
-        ALOG << "Hello" << " World " << 3.2 << " xuku";
+        ALOG << "Hello" << " World " << 3.2 << " blabla " << i;
         usleep(1);
     }
     FILE_LOG(logINFO) << "Finishing logging " << NUM << " messages";
